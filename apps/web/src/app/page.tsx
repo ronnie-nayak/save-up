94b35f2087803bd06061032bcf7e1b42dff62564
Ronnie Nayak <abhishek.ron.nayak@gmail.com>
2024-01-01 16:21:59 +0530

    lazygit test

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Page(): JSX.Element {
   return (
     <main className="dark">
       <h1 className="m-8 text-xl font-bold">Hello Ron!</h1>
+
       <Hero />
       <Chart />
       <Savings />
