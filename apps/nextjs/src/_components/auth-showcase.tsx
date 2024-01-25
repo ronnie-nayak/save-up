aa6e67378a4e7a5e11a07ad819aa2529eb01ddf3
Ronnie Nayak <abhishek.ron.nayak@gmail.com>
2024-01-25 14:31:59 +0530

    format

--- apps/nextjs/src/_components/auth-showcase.tsx
+++ apps/nextjs/src/_components/auth-showcase.tsx
@@ -15,7 +15,7 @@ export async function AuthShowcase() {
         }}
       ></div>
       <form
-        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-2/6 sm:w-2/6 rounded-3xl bg-white p-8  flex flex-col"
+        className="absolute left-1/2 top-1/2 flex h-2/6 -translate-x-1/2 -translate-y-1/2 flex-col rounded-3xl bg-white  p-8 sm:w-2/6"
         style={{
           borderRadius: "50px",
           boxShadow: "0px 0px 20px 0px rgba(0,0,0,0.75)",
@@ -26,12 +26,12 @@ export async function AuthShowcase() {
         {/*     <Loading /> */}
         {/*   </div> */}
         {/* ) : */}
-        {(
-          <div className="flex flex-col justify-center h-full w-full">
-            <div className="flex items-center justify-center gap-2 h-4/6">
+        {
+          <div className="flex h-full w-full flex-col justify-center">
+            <div className="flex h-4/6 items-center justify-center gap-2">
               <button
                 key="Github"
-                className="text-black font-bold py-4 px-4 h-3/4 rounded border border-black w-5/12 flex justify-center items-center sm:text-[1.5vw]"
+                className="flex h-3/4 w-5/12 items-center justify-center rounded border border-black px-4 py-4 font-bold text-black sm:text-[1.5vw]"
                 formAction={async () => {
                   "use server";
                   await signIn("github", {
@@ -45,7 +45,7 @@ export async function AuthShowcase() {
 
               <button
                 key="Discord"
-                className="text-black font-bold py-4 px-4 h-3/4 rounded border border-black w-5/12 flex justify-center items-center sm:text-[1.5vw]"
+                className="flex h-3/4 w-5/12 items-center justify-center rounded border border-black px-4 py-4 font-bold text-black sm:text-[1.5vw]"
                 formAction={async () => {
                   "use server";
                   await signIn("discord", {
@@ -57,9 +57,9 @@ export async function AuthShowcase() {
                 <h2>Discord</h2>
               </button>
             </div>
-            <div className="h-full flex flex-col justify-around items-center">
+            <div className="flex h-full flex-col items-center justify-around">
               <Button
-                className="w-3/4 h-[16vw] sm:h-[5vw] sm:text-[1.75vw] font-bold"
+                className="h-[16vw] w-3/4 font-bold sm:h-[5vw] sm:text-[1.75vw]"
                 formAction={async () => {
                   "use server";
                   await signIn("credentials", {
@@ -70,12 +70,13 @@ export async function AuthShowcase() {
                 Guest User
               </Button>
 
-              <h2 className=" w-max mx-auto my-5 text-[4vw] sm:text-[1.5vw] font-bold">Kindly wait 5-10 sec for Login to complete</h2>
+              <h2 className=" mx-auto my-5 w-max text-[4vw] font-bold sm:text-[1.5vw]">
+                Kindly wait 5-10 sec for Login to complete
+              </h2>
             </div>
           </div>
-        )
         }
-      </form >
-    </div >
+      </form>
+    </div>
   );
 }
